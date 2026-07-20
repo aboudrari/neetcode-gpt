8f72a493d02677c0f711ac38812b80d24331f42e
ABDALLAH ABOUDRARI <aboudrariabdallah1@gmail.com>
2026-07-20 16:08:40 +0300

    Implement Batch normalization (PyTorch 3/4)

---
+++ model/batch_normalization.py
@@ -0,0 +1,36 @@
+import numpy as np
+from typing import Tuple, List
+
+
+class Solution:
+    def batch_norm(self, x: List[List[float]], gamma: List[float], beta: List[float],
+                   running_mean: List[float], running_var: List[float],
+                   momentum: float, eps: float, training: bool) -> Tuple[List[List[float]], List[float], List[float]]:
+        # During training: normalize using batch statistics, then update running stats
+        # During inference: normalize using running stats (no batch stats needed)
+        # Apply affine transform: y = gamma * x_hat + beta
+        # Return (y, running_mean, running_var), all rounded to 4 decimals as lists
+        x = np.array(x)
+        gamma = np.array(gamma)
+        beta = np.array(beta)
+        running_mean = np.array(running_mean)
+        running_var = np.array(running_var)
+        mean = np.mean(x, axis=0)  # axis = 0, across the batches, column 
+        N = x.shape[0]  # len of the rows 
+        var = np.sum((x - mean)**2, axis=0) / N
+        if training:
+            x_hat = (x - mean) / np.sqrt(var + eps)
+            running_mean = (1 - momentum) * running_mean + momentum * mean
+            running_var = (1 - momentum) * running_var + momentum * var
+        else :
+            x_hat = (x - running_mean) / np.sqrt(running_var + eps)
+
+        y = gamma * x_hat + beta
+        return (
+            np.round(y, 4).tolist(),
+            np.round(running_mean, 4).tolist(),
+            np.round(running_var, 4).tolist()
+        )
+        
+
+
